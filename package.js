9b5149a51ffc53b87422271be9b3ef3574fa1ade
Phil Cockfield <phil@cockfield.net>
2014-07-04 09:19:00 -0700

    Lay down files for ScopedSession

--- package.js
+++ package.js
@@ -13,6 +13,7 @@ Package.on_use(function (api) {
   api.add_files('shared/_export.coffee', ['client', 'server']);
   api.add_files('shared/handlers.coffee', ['client', 'server']);
   api.add_files('shared/reactive-hash.coffee', ['client', 'server']);
+  api.add_files('shared/scoped-session.coffee', ['client', 'server']);
   api.add_files('shared/timer.coffee', ['client', 'server']);
   api.add_files('shared/util.coffee', ['client', 'server']);
 
@@ -26,9 +27,9 @@ Package.on_test(function (api) {
 
   // Generated with: github.com/philcockfield/meteor-package-loader
   api.add_files('tests/shared/_init.coffee', ['client', 'server']);
-
   api.add_files('tests/shared/handlers-test.coffee', ['client', 'server']);
   api.add_files('tests/shared/reactive-hash-test.coffee', ['client', 'server']);
+  api.add_files('tests/shared/scoped-session-test.coffee', ['client', 'server']);
   api.add_files('tests/shared/timer-test.coffee', ['client', 'server']);
   api.add_files('tests/shared/util-test.coffee', ['client', 'server']);
 
---
+++ tests/shared/scoped-session-test.coffee
@@ -0,0 +1,4 @@
+describe 'ScopedSession', ->
+  it 'constructs with namespace', ->
+    console.log 'TODO ScopedSession'
+
